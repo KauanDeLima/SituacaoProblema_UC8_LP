54223d342a5b0a52746c79110b20b12220d21dd5
KauanDeLima <101983769+KauanDeLima@users.noreply.github.com>
2022-04-14 14:54:53 -0300

    pronta

---
+++ situacaoProblema.js
@@ -0,0 +1,19 @@
+// situação problema uc8
+
+
+
+
+let alunos = 38;
+
+for (let i = 0; i <= alunos ; i++) {
+    
+        if (i==0) {
+            console.log(`zero: ${i}`)           
+        } 
+        else if (i % 2 ==0) {
+            console.log(`PAR: ${i}`);        
+    }
+        else {
+            console.log(`Impar: ${i}`);
+        }
+      }
